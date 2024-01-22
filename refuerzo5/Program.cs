f5a20c37e12858c6f06645441c5616b27638d03e
isaac <isaac@ISAC>
2024-01-21 21:03:26 -0400

    Agregar archivos de proyecto.

---
+++ refuerzo5/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Introduzca el número x: ");
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= x; i++)
+{
+    Console.WriteLine(CardinalToOrdinal(i));
+}
+
+static string CardinalToOrdinal(int cardinal)
+
+{
+    string ordinal = cardinal.ToString();
+
+    if (cardinal % 10 == 1)
+    {
+        ordinal += "st";
+    }
+    else if (cardinal % 10 == 2)
+    {
+        ordinal += "nd";
+    }
+    else if (cardinal % 10 == 3)
+    {
+        ordinal += "rd";
+    }
+    else
+    {
+        ordinal += "th";
+    }
+
+    return ordinal;
+}
